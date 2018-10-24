5dbd78496a0ec75430359dcdb2aafafe9d441de8
David Lacho <david.lacho@gmail.com>
2018-10-23 19:42:30 -0700

    removed commenting from planning phase of project

--- character-counting.js
+++ character-counting.js
@@ -1,20 +1,18 @@
 countLetters = (input) => {
-  input = input.split(' ').join(''); 
+  input = input.split(' ').join('');
   let returnObject = {}
   for (i = 0; i < input.length; i++) {
     let characterToCheck = input[i];
     let characterCount = 0;
-    for (j=0; j < input.length; j++) {
+    for (j = 0; j < input.length; j++) {
       if (input[j] === characterToCheck) {
-        characterCount ++;
+        characterCount++;
       }
     }
     returnObject[characterToCheck] = [characterCount];
   }
 
   return returnObject;
-  // return to us all the unique characters that exist in a string that is passed into the function.
-  // the function should also report back how many instances of each letter were found in the string.
 }
 
 
